3da24e3b48b09fb4b085096df0dc4211c43f825f
Igor Korsikov <korsikov009@gmail.com>
2018-06-08 11:08:38 +0300

    Изменена реализация переходов (белые блоки под наклоном)

--- src/js/mix-list.js
+++ src/js/mix-list.js
@@ -6,9 +6,9 @@
 
 	    list.appendChild(listLi[1]);
 	    list.appendChild(listLi[0]);
-	    list.appendChild(listLi[3]);
-	    list.appendChild(listLi[4]);
-	    list.appendChild(listLi[2]);
+	    list.appendChild(listLi[1]);
+	    list.appendChild(listLi[1]);
+	    list.appendChild(listLi[0]);
 	}
 
 	if (document.documentElement.clientWidth < 999) {
